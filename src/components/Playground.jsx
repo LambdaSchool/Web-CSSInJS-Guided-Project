95dc02b2be64411033d14cd7a934b86bae2b017b
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-06 11:11:00 +0200

    wip

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const Div = styled.div`
   font-weight: bold;
+
+  h3.pinky {
+    color: pink;
+  }
 `;
 
-export default function Playground({ primaryColor, secondaryColor }) {
+export default function Playground({ }) {
   return (
     <Div
       className='alert'
     >
-      <h3>
+      <h3 className='pinky'>
         This is the playground component
       </h3>
 
---
+++ src/styled/Div.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  font-weight: bold;
+
+  h3.pinky {
+    color: pink;
+  }
+`;
